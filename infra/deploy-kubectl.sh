ef0901a9a589b1db065bd372cabeca9117bafa52
Sebastián Ramírez <tiangolo@gmail.com>
2024-07-09 23:19:43 -0500

    ✨ Add Knative setup (including Kourier, TLS certificates, default custom domain, semi-automatic install with Kustomize) (#139)
    * 🚚 Move cert-manager manifests to their own directory
    
    * 🔧 Add Pulumi configs for production
    
    * 🔧 Add Kustomize patch to set Kourier as the ingress class for Knative
    
    * 🔧 Add Kustomize patch to configure the AWS Load Balancer for Knative's Kourier
    
    * 🔧 Add Kustomize patch to set up the domain TLS certificate (generated by cert-manager)
    
    * 🔧 Update Knative domain template, to not include namespace, only app name
    
    * 🔧 Add Kustomize for Knative and Kourier
    
    * 🔧 Add Kustomize configs for domain in staging
    
    * 🔧 Add Kustomize patch for production domain
    
    * 🔨 Update script to deploy Kubernetes resources, including Knative

--- infra/deploy-kubectl.sh
+++ infra/deploy-kubectl.sh
@@ -13,17 +13,36 @@ export KUBECONFIG=kubeconfig.json
 # Enable debugging
 set -x
 
+KNATIVE_VERSION="1.14.1"
+
 echo "Add Cloudflare token secret"
-envsubst < k8s/cert-manager-cloudflare-token.yaml | kubectl apply -f -
+envsubst < k8s/cert-manager/cloudflare-token.yaml | kubectl apply -f -
 
 echo "Add Cert Manager issuer prod"
-kubectl apply -f k8s/cert-manager-issuer-prod.yaml
+kubectl apply -f k8s/cert-manager/issuer-prod.yaml
 
 echo "Add Cert Manager issuer staging"
-kubectl apply -f k8s/cert-manager-issuer-staging.yaml
+kubectl apply -f k8s/cert-manager/issuer-staging.yaml
 
 echo "Add wildcard cert"
-kubectl apply -f "k8s/cert-manager-wildcard-cert-${DEPLOY_ENVIRONMENT}.yaml"
+kubectl apply -f "k8s/cert-manager/wildcard-cert-${DEPLOY_ENVIRONMENT}.yaml"
+
+echo "Add wildcard cert for serving"
+kubectl apply -f "k8s/cert-manager/wildcard-cert-serving-${DEPLOY_ENVIRONMENT}.yaml"
+
+echo "Install Knative Serving CRDs"
+KNATIVE_TAG="knative-v${KNATIVE_VERSION}"
+kubectl apply -f https://github.com/knative/serving/releases/download/${KNATIVE_TAG}/serving-crds.yaml
+
+echo "Install Knative Serving and Kourier"
+kubectl apply -k "k8s/overlays/${DEPLOY_ENVIRONMENT}"
+
+echo "Add DNS record for Knative:"
+
+KNATIVE_LOAD_BALANCER_HOSTHAME=$(kubectl -n kourier-system get service kourier --output jsonpath='{.status.loadBalancer.ingress[0].hostname}')
+
+echo "Add DNS record for Knative Load Balancer before continuing:"
+echo "$KNATIVE_LOAD_BALANCER_HOSTHAME"
 
 echo "Remove kubeconfig.json with by running:"
 echo "rm kubeconfig.json"
