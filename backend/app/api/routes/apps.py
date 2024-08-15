a44d81e22e8151f0624948110b3e4fab6b534b41
User <alejsdev@gmail.com>
2024-08-14 19:08:08 -0500

    Add endpoint to read apps

--- backend/app/api/routes/apps.py
+++ backend/app/api/routes/apps.py
@@ -1,6 +1,7 @@
-from typing import Any
+from typing import Any, Literal
 
 from fastapi import APIRouter, HTTPException
+from sqlmodel import col, func, select
 
 from app.api.deps import CurrentUser, SessionDep
 from app.api.utils.teams import generate_app_slug_name
@@ -9,9 +10,50 @@ from app.models import (
     App,
     AppCreate,
     AppPublic,
+    AppsPublic,
 )
 
 router = APIRouter()
+
+
+@router.get("/", response_model=AppsPublic)
+def read_apps(
+    session: SessionDep,
+    current_user: CurrentUser,
+    team_slug: str,
+    skip: int = 0,
+    limit: int = 100,
+    order_by: Literal["created_at"] | None = None,
+    order: Literal["asc", "desc"] = "asc",
+) -> Any:
+    """
+    Retrieve a list of apps for the provided team.
+    """
+    count_statement = select(func.count()).select_from(App)
+    statement = select(App).offset(skip).limit(limit)
+
+    user_team_link = get_user_team_link_by_user_id_and_team_slug(
+        session=session, user_id=current_user.id, team_slug=team_slug
+    )
+    if not user_team_link:
+        raise HTTPException(
+            status_code=404, detail="Team not found for the current user"
+        )
+
+    statement = statement.where(App.team_id == user_team_link.team_id)
+    count_statement = count_statement.where(App.team_id == user_team_link.team_id)
+
+    order_key = col(App.created_at) if order_by == "created_at" else None
+
+    if order_key:
+        statement = statement.order_by(
+            order_key.asc() if order == "asc" else order_key.desc()
+        )
+
+    apps = session.exec(statement).all()
+    count = session.exec(count_statement).one()
+
+    return AppsPublic(data=apps, count=count)
 
 
 @router.post("/", response_model=AppPublic)
