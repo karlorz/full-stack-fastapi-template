f2bbf2d913f64acfb7b222be41bb9790e9485a1b
Sebastián Ramírez <tiangolo@gmail.com>
2024-09-23 22:58:58 +0200

    ♻️ Refactor `generate-client.sh` to error out on failures and make settings use top level `.env` (#343)

--- scripts/generate-client.sh
+++ scripts/generate-client.sh
@@ -1,8 +1,13 @@
 #! /usr/bin/env bash
 
-PYTHONPATH=backend python -c "import app.main; import json; print(json.dumps(app.main.app.openapi()))" > openapi.json
+set -e
+set -x
+
+cd backend
+python -c "import app.main; import json; print(json.dumps(app.main.app.openapi()))" > ../openapi.json
+cd ..
 node frontend/modify-openapi-operationids.js
 mv openapi.json frontend/
-cd frontend || exit
+cd frontend
 npm run generate-client
 npx biome format --write ./src/client
