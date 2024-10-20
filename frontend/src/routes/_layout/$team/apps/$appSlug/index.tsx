046993866e30be265aa82e0c1880ddcd6ec8c6f0
Alejandra <90076947+alejsdev@users.noreply.github.com>
2024-10-20 18:24:32 +0100

    ♻️ Refactor app detail and add `RefetchInterval` to deployments query (#441)

--- frontend/src/routes/_layout/$team/apps/$appSlug/index.tsx
+++ frontend/src/routes/_layout/$team/apps/$appSlug/index.tsx
@@ -1,18 +1,7 @@
 import { ExternalLinkIcon } from "@chakra-ui/icons"
-import {
-  Box,
-  Button,
-  Container,
-  Flex,
-  Heading,
-  Link,
-  Skeleton,
-  Text,
-} from "@chakra-ui/react"
-import { useQuery, useQueryClient } from "@tanstack/react-query"
-import { createFileRoute, useNavigate } from "@tanstack/react-router"
-import { useEffect } from "react"
-import { z } from "zod"
+import { Box, Container, Heading, Link, Skeleton, Text } from "@chakra-ui/react"
+import { useQuery } from "@tanstack/react-query"
+import { createFileRoute } from "@tanstack/react-router"
 
 import { AppsService, DeploymentsService } from "@/client"
 import DeleteApp from "@/components/AppSettings/DeleteApp"
@@ -21,47 +10,9 @@ import EnvironmentVariables from "@/components/AppSettings/EnvironmentVariables"
 import CustomCard from "@/components/Common/CustomCard"
 import { fetchTeamBySlug } from "@/utils"
 
-const deploymentsSearchSchema = z.object({
-  page: z.number().catch(1).optional(),
-  orderBy: z.enum(["created_at"]).optional(),
-  order: z.enum(["asc", "desc"]).optional(),
-})
-
-const PER_PAGE = 5
-
-function getDeploymentsQueryOptions({
-  page,
-  orderBy = "created_at",
-  order = "desc",
-  appId,
-}: {
-  page: number
-  orderBy?: "created_at"
-  order?: "asc" | "desc"
-  appId: string
-}) {
-  return {
-    queryFn: () =>
-      DeploymentsService.readDeployments({
-        skip: (page - 1) * PER_PAGE,
-        limit: PER_PAGE + 1,
-        orderBy,
-        order,
-        appId,
-      }),
-    queryKey: ["deployments", { page, orderBy, order, appId }],
-  }
-}
-
 export const Route = createFileRoute("/_layout/$team/apps/$appSlug/")({
   component: AppDetail,
-  validateSearch: (search) => deploymentsSearchSchema.parse(search),
-  loaderDeps: ({ search: { page, orderBy, order } }) => ({
-    page,
-    orderBy,
-    order,
-  }),
-  loader: async ({ context, params, deps }) => {
+  loader: async ({ context, params }) => {
     const team = await fetchTeamBySlug(params.team)
 
     const apps = await AppsService.readApps({
@@ -69,14 +20,12 @@ export const Route = createFileRoute("/_layout/$team/apps/$appSlug/")({
       slug: params.appSlug,
     })
 
-    const deployments = await context.queryClient.fetchQuery(
-      getDeploymentsQueryOptions({
-        appId: apps.data[0].id,
-        page: deps.page || 1,
-        orderBy: deps.orderBy,
-        order: deps.order,
-      }),
-    )
+    const deployments = await DeploymentsService.readDeployments({
+      appId: apps.data[0].id,
+      orderBy: "created_at",
+      order: "desc",
+      limit: 5,
+    })
 
     const app = apps.data[0]
 
@@ -97,34 +46,20 @@ export const Route = createFileRoute("/_layout/$team/apps/$appSlug/")({
 })
 
 function AppDetail() {
-  const { page = 1, order, orderBy } = Route.useSearch()
-  const navigate = useNavigate({ from: Route.fullPath })
-  const setPage = (page: number) =>
-    navigate({ search: (prev: any) => ({ ...prev, page }) })
-  const queryClient = useQueryClient()
+  const { app, deployments: initialDeployments } = Route.useLoaderData()
 
-  const {
-    app,
-    deployments: { data },
-  } = Route.useLoaderData()
-
-  const hasNextPage = data.length === PER_PAGE + 1
-  const deployments = data.slice(0, PER_PAGE)
-
-  const hasPreviousPage = page > 1
-
-  useEffect(() => {
-    if (hasNextPage) {
-      queryClient.prefetchQuery(
-        getDeploymentsQueryOptions({
-          page: page + 1,
-          orderBy,
-          order,
-          appId: app.id,
-        }),
-      )
-    }
-  }, [page, queryClient, hasNextPage, order, orderBy, app.id])
+  const { data: deployments } = useQuery({
+    queryKey: ["deployments", app.id],
+    queryFn: () =>
+      DeploymentsService.readDeployments({
+        appId: app.id,
+        orderBy: "created_at",
+        order: "desc",
+        limit: 5,
+      }),
+    initialData: initialDeployments,
+    refetchInterval: 1000,
+  })
 
   const { data: environmentVariables } = useQuery({
     queryKey: ["apps", app.id, "environmentVariables"],
@@ -146,30 +81,7 @@ function AppDetail() {
         <Text>Last Updated: {new Date(app?.updated_at).toLocaleString()}</Text>
         <Box pt={10}>
           <CustomCard title="Deployments">
-            <Deployments deployments={deployments} />
-            {(hasPreviousPage || hasNextPage) && (
-              <Flex
-                gap={4}
-                alignItems="center"
-                mt={4}
-                direction="row"
-                justifyContent="flex-end"
-              >
-                <Button
-                  onClick={() => setPage(page - 1)}
-                  isDisabled={!hasPreviousPage}
-                >
-                  Previous
-                </Button>
-                <span>Page {page}</span>
-                <Button
-                  isDisabled={!hasNextPage}
-                  onClick={() => setPage(page + 1)}
-                >
-                  Next
-                </Button>
-              </Flex>
-            )}
+            <Deployments deployments={deployments.data} />
           </CustomCard>
 
           <CustomCard title="Environment Variables">
